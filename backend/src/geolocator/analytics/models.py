6c31b5a4fbe6dde6c4eb56247a8854e064e3982a
shivanXI <shivanstar7@gmail.com>
2017-06-13 10:02:23 +0530

    Added user ip tracking in analytics feature

--- backend/src/geolocator/analytics/models.py
+++ backend/src/geolocator/analytics/models.py
@@ -4,6 +4,32 @@ from django.db import models
 
 from .signals import user_logged_in
 # Create your models here.
+
+
+class UserSessionManager(models.Manager):
+	def create_new(self, user, session_key=None, ip_address=None, city_data=None):
+		session_new = self.model()
+		session_new.user = user
+		session_new.session_key = session_key
+		if ip_address is not None:
+			session_new.ip_address = ip_address
+			if city_data:
+				session_new.city_data = city_data
+				try:
+					city = city_data['city']
+				except:
+					city = None
+				session_new.city = city_data
+				try:
+					country = city_data['country_name']
+				except:
+					country = None
+			session_new.country = country
+			session_new.save()
+			return session_new
+		return None
+
+
 class UserSession(models.Model):
 	user 			= models.ForeignKey(settings.AUTH_USER_MODEL)
 	session_key 	= models.CharField(max_length=60, null=True, blank=True)
