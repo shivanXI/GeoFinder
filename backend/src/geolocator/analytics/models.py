f4637c4575426abdc646c50b8b76cd3d82480daf
shivanXI <shivanstar7@gmail.com>
2017-06-14 20:56:42 +0530

    Added yelp API searching feature to the app for searching any specific thing near about you

--- backend/src/geolocator/analytics/models.py
+++ backend/src/geolocator/analytics/models.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
+
 from django.conf import settings
 from django.db import models
 
 from .signals import user_logged_in
+from .utils import get_client_city_data, get_client_ip
 # Create your models here.
+
 
 
 class UserSessionManager(models.Manager):
@@ -30,6 +33,7 @@ class UserSessionManager(models.Manager):
 		return None
 
 
+
 class UserSession(models.Model):
 	user 			= models.ForeignKey(settings.AUTH_USER_MODEL)
 	session_key 	= models.CharField(max_length=60, null=True, blank=True)
@@ -40,16 +44,41 @@ class UserSession(models.Model):
 	active 			= models.BooleanField(default=True)
 	timestamp 		= models.DateTimeField(auto_now_add=True)
 
+	objects  = UserSessionManager()
+
 	def __str__(self):
-		if self.city_data:
-			return str(self.city_data)
+		#if self.city_data:
+		#	return str(self.city_data)
+		city = self.city
+		country = self.country
+		if city and country:
+			return f"{city}, {country}"
+		elif city and not country:
+			return f"{city}"
+		elif country and not city:
+			return f"{country}"
 		return self.user.username
+
+
 
 def user_logged_in_receiver(sender, request, *args, **kwargs):
 	user = sender
 	#request = #parse the request to get the ip address	
+	#city_data = None
+	ip_address = get_client_ip(request)
+	#ip_address = '72.14.207.99'
+	#if ip_address:
+	#	city_data = get_client_city_data(ip_address)
 	session_key = request.session.session_key #got ip address
-	UserSession.objects.create(user = user, session_key = session_key)
+	#UserSession.objects.create(user = user,
+	UserSession.objects.create_new(
+								user = user, 
+								session_key = session_key,
+								ip_address = ip_address
+								#city_data = city_data
+							    #) 
+								)
+
 
 
 user_logged_in.connect(user_logged_in_receiver)
